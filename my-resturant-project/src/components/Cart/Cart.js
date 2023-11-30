89b2152e02b43679daef830da50a995b8b6d7bc9
Gunashekar <Gunashekarreddy057@gmail.com>
2023-11-30 13:57:16 +0530

    Cartbtn

--- my-resturant-project/src/components/Cart/Cart.js
+++ my-resturant-project/src/components/Cart/Cart.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from './Cart.module.css'
 import Modal from "../UI/Modal";
-const Cart = () => {
+const Cart = (props) => {
   const CartItem = (
         <ul className={classes['cart-items']}>
           {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
@@ -10,14 +10,14 @@ const Cart = () => {
         </ul>
       );
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
     {CartItem}
     <div className={classes.total}>
       <span>Total Amount</span>
       <span>35.62</span>
     </div>
     <div className={classes.actions}>
-      <button className={classes['button--alt']}>Close</button>
+      <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
       <button className={classes.button}>Order</button>
     </div>
   </Modal>
